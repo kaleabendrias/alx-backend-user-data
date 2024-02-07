c23087f983b219408c90c3e0279ff674d27c93d9
kaleab <kaleabendrias1212@gmail.com>
2024-02-07 21:33:18 +0300

    Refactor log message redaction in filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,11 +18,12 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
         log_message = super().format(record)
         for field in self.fields:
-            log_message = re.sub(r'{}=.*?(?=;)'.format(field), '{}={}'.format(field, self.REDACTION), log_message)
+            log_message = re.sub(r'{}=.*?(?=;)'.format(field),
+                                 '{}={}'.format(field, self.REDACTION),
+                                 log_message)
         return log_message
 
 
@@ -37,5 +38,6 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
     logger.propagate = False
     return logger
+
 
 PII_FIELDS: List[str] = ['name', 'email', 'phone', 'ssn', 'credit_card']
