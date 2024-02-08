5620078976aeef63ef2f15980b0eed47b5dee853
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-08 11:36:41 +0300

    Add type hinting to get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,7 +31,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Returns a connector to the database."""
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
