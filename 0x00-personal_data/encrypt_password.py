5b9a1ad64b5e05fe831ab93a3dc99d4ec0194abd
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-08 12:46:48 +0300

    Add password validation function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,10 @@ def hash_password(password: str) -> bytes:
     password = password.encode('utf-8')
     hashed = bcrypt.hashpw(password, bcrypt.gensalt(14))
     return hashed
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """to validate that the provided password matches the hashed password."""
+    password = password.encode('utf-8')
+    if bcrypt.checkpw(password, hashed_password):
+        return True
+    return False
