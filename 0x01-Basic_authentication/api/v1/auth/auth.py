1b61879ba68e78b8a23b4a4b2e88f16391e5790d
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-12 23:20:22 +0300

    Add logic to require authentication based on path and excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,13 @@ class Auth:
     """class named Auth"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth"""
-        pass
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """that returns None - request will be the Flask request object"""
