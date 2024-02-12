0509436c7f162f70c94f0368c47ba2de4b8aaa09
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-13 01:28:55 +0300

    Fix authentication initialization in app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ if os.getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
 elif os.getenv('AUTH_TYPE') == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
