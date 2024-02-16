d23f0cd879b9cdec738ac1bda17f9dbcd81082e0
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-16 10:09:35 +0300

    Fix authentication logic in before_request() function

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,12 +34,12 @@ def before_request():
                                         '/api/v1/forbidden/',
                                         '/api/v1/auth_session/login/']):
 
+        if auth.authorization_header(request) and auth.session_cookie(request):
+            return None, abort(401)
         if not auth.authorization_header(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)
-        if auth.authorization_header(request) and auth.session_cookie(request):
-            return None, abort(401)
         request.current_user = auth.current_user(request)
     return
 
