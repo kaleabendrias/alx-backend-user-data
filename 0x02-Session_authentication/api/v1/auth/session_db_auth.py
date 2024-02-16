35bab45b75ae74f4e69cbc31b21b78714107e39e
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-16 13:45:54 +0300

    Refactor session_db_auth.py for improved session management

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,6 +2,7 @@
 """session db auth"""
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -11,7 +12,8 @@ class SessionDBAuth(SessionExpAuth):
         session_id = super().create_session(user_id)
         if not session_id:
             return None
-        user_session = UserSession({"user_id": user_id, "session_id": session_id})
+        user_session = UserSession({"user_id": user_id,
+                                    "session_id": session_id})
         user_session.save()
         user_session.save_to_file()
         return session_id
@@ -24,18 +26,23 @@ class SessionDBAuth(SessionExpAuth):
         user_session = UserSession.search({'session_id': session_id})
         if not user_session:
             return None
+        expiry_time = user_session.created_at + timedelta(
+            seconds=self.session_duration)
+        if expiry_time < datetime.utcnow():
+            return None
+
         return user_session[0].user_id
 
     def destroy_session(self, request=None):
         """destroys the UserSession based on the Session ID"""
         if not request:
-            return False
+            return None
         session_id = self.session_cookie(request)
         if not session_id or not self.user_id_for_session_id(session_id):
-            return False
+            return None
         user_session = UserSession.search({"session_id": session_id})
         if not user_session:
-            return False
+            return None
         user_session[0].remove()
         UserSession.save_to_file()
         return True
