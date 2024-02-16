80813697d409e50c2d6c3f687fcbc4aeddf71479
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-16 10:00:41 +0300

    Add login endpoint to require authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,12 +31,15 @@ def before_request():
         return
     if auth.require_auth(request.path, ['/api/v1/status/',
                                         '/api/v1/unauthorized/',
-                                        '/api/v1/forbidden/']):
+                                        '/api/v1/forbidden/',
+                                        '/api/v1/auth_session/login/']):
 
         if not auth.authorization_header(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)
+        if auth.authorization_header(request) and auth.session_cookie(request):
+            return None, abort(401)
         request.current_user = auth.current_user(request)
     return
 
