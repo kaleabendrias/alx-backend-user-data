8700f185caccb170caa0d049681af03e473ea4ec
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-16 10:10:42 +0300

    Fix session_id generation in SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,7 +16,7 @@ class SessionAuth(Auth):
         """creates a Session ID for a user_id"""
         if user_id is None or type(user_id) != str:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
