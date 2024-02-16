8efbec2febe8f009f115271dda8d2e77b3426746
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-16 10:16:34 +0300

    Fix authentication logic in app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,7 +34,7 @@ def before_request():
                                         '/api/v1/forbidden/',
                                         '/api/v1/auth_session/login/']):
 
-        if auth.authorization_header(request) and auth.session_cookie(request):
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
             abort(401)
         if not auth.authorization_header(request):
             abort(401)
