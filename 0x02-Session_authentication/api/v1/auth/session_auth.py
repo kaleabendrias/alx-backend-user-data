46f6e166af3fcef61329541402bde69b2db72474
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-16 10:29:57 +0300

    Add current_user method to SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,10 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value"""
+        from models.user import User
+        cookie_name = self.session_cookie(request)
+        userId = self.user_id_for_session_id(cookie_name)
+        return User.get(userId)
