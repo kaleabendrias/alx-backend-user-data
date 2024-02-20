a1ca61eb0f5f6ce57e611740e10d292a0406f53e
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-20 16:32:49 +0300

    Add profile route for user profile retrieval

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,17 @@ def logout():
     abort(403)
 
 
+@app.route('/profile')
+def profile():
+    """request is expected to contain a session_id cookie.
+    Use it to find the user.If the user exist, respond with
+    a 200 HTTP status and the following JSON payload:"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
