ec9bf999c75b91915e3ab330ba1e5b34b950c235
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-20 10:39:12 +0300

    Remove unused code in User class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,8 +9,6 @@ Base = declarative_base()
 
 class User(Base):
     """this is class User"""
-    __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
