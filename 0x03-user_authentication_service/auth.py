b6302b1bf2ed430922a8604980e560a110155b13
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-20 13:03:36 +0300

    Add UUID generation function***
    This commit adds a new function `_generate_uuid()` to the `auth.py` file. The function generates a string representation of a new UUID using the `uuid4()` function from the `uuid` module. This function will be used for generating unique identifiers in the authentication process.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,11 +4,17 @@ from db import DB
 import bcrypt
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
     """takes in a password string arguments and returns bytes."""
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """should return a string representation of a new UUID"""
+    return str(uuid4())
 
 
 class Auth:
