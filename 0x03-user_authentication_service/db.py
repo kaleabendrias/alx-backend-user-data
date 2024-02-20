b2a1dc43fccaca42837caa4e618944f8b3375c4e
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-20 11:26:50 +0300

    Add type hinting to find_user_by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from typing import Dict
 
 from user import Base, User
 
@@ -39,7 +40,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs: Dict[str, str]) -> User:
         """ returns the first row found in the users"""
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
