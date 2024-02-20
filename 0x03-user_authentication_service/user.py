64428274f289528fc57c79288c3ee3eeba20263f
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-20 10:43:10 +0300

    Add tablename attribute to User class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,6 +9,8 @@ Base = declarative_base()
 
 class User(Base):
     """this is class User"""
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
