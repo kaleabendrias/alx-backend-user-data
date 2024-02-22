124e237cc6f1b5b69871214538b6874ed08e94ee
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-22 16:19:47 +0300

    changes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,6 +72,43 @@ def profile():
         return jsonify({"email": user.email}), 200
     abort(403)
 
+@app.route('/reset_password', methods=['POST'])
+def reset_password() -> str:
+    """respond POST /reset_password route."""
+    try:
+        email = request.form['email']
+    except KeyError:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "reset_token": reset_token}
+
+    return jsonify(msg), 200
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    """ respond PUT /reset_password route.
+    """
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+    except KeyError:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    msg = {"email": email, "message": "Password updated"}
+    return jsonify(msg), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
