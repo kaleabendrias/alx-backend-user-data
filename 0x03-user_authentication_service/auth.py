88c0bf4a425a7c6469c68ef93f70d39874da4813
kaleabendrias <kaleabendrias1212@gmail.com>
2024-02-22 16:09:41 +0300

    changes

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,28 +28,28 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Return a User object."""
         try:
-            user = self._db.find_user_by(email=email)
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists.")
         except NoResultFound:
-            hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-
-            return user
-
-        else:
-            raise ValueError(f'User {email} already exists')
-
+            hashed_pwd = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_pwd)
+        return new_user
 
     def valid_login(self, email: str, password: str) -> bool:
         """validates login"""
         try:
             user = self._db.find_user_by(email=email)
-            if user:
-                password = bytes(password, "utf-8")
-                if bcrypt.checkpw(password, user.hashed_password):
-                    return True
-                return False
         except NoResultFound:
             return False
+
+        user_password = user.hashed_password
+        encoded_password = password.encode()
+
+        if bcrypt.checkpw(encoded_password, user_password):
+            return True
+
+        return False
 
     def create_session(self, email: str) -> str:
         """find the user corresponding to the email,
